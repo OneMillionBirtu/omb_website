9ab7dfa35651561ca12ac1314b9a78ab36d2a9ea
EyosiSol <eyosila123@gmail.com>
2025-11-01 11:37:37 +0300

    feat: add linting and prettier for CMS

--- cms/eslint.config.mjs
+++ cms/eslint.config.mjs
@@ -1,18 +1,36 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.mjs
+import next from 'eslint-config-next';
+import prettier from 'eslint-config-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const config = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', '.next', 'out', 'dist'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': 'warn'
+    }
+  },
+  ...next,
+  prettier
+];
 
-export default eslintConfig;
+export default config;
